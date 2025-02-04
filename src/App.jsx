bb489a49bee6f13614832b5a888933b7a88de7c4
ChristianAoC <christianaoc@hcac.de>
2025-02-04 17:20:34 +0000

    added some temp simulation

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,53 @@ import './App.css'
 import '@mantine/core/styles.css';
 
 export default function App() {
-  const [value, setThermostat] = useState(270);
+  const [indoorTemp, setIndoorTemp] = useState(21);
+  const [outdoorTemp, setOutdoorTemp] = useState(10);
+  const [outdoorTempMax, setOutdoorTempMax] = useState(22);
+  const [outdoorTempMin, setOutdoorTempMin] = useState(-8);
+  const [lowerComf, setLowerComf] = useState(18);
+  const [higherComf, setHigherComf] = useState(24);
+
+  // https://www.h2xengineering.com/blogs/calculating-heat-loss-simple-understandable-guide/
+  const [heatLoss, setHeatLoss] = useState(0.1); // heat loss per hour per degree - 0.1 would be more realistic but "too slow".
+
+  const [thermostat, setThermostat] = useState(270);
+  const thermToHeat = {
+    270: 0,
+    315: 0.5,
+    345: 1,
+     15: 1.75,
+     45: 2.5,
+     75: 3.5
+  }
 
   const [gamePaused, setGamePaused] = useState(false);
-  const [gameStepSize, setGameStepSize] = useState(1000);
+  const [gameStepSize, setGameStepSize] = useState(10); // 1000 is one second ticks
   const [gameTurn, setGameTurn] = useState(24);
 
   useEffect(() => {
-    const timer = setTimeout(() => !gamePaused && setGameTurn(gameTurn+1), gameStepSize)
+    const timer = setTimeout(() => {
+      if (!gamePaused) {
+        setGameTurn(gameTurn+1)
+
+        // calculate new outdoortemp
+        var newOutTemp = outdoorTemp + Math.floor((Math.random()*3)-1)
+        if (newOutTemp < outdoorTempMin) { setOutdoorTemp(outdoorTempMin) }
+        else if (newOutTemp > outdoorTempMax) { setOutdoorTemp(outdoorTempMax) }
+        else { setOutdoorTemp(newOutTemp) }
+        setOutdoorTemp(newOutTemp)
+
+        // calculate new indoortemp
+        var newInTemp = indoorTemp // old indoor temp
+        newInTemp += (outdoorTemp-indoorTemp) * heatLoss // temp lost to outside
+        newInTemp += thermToHeat[thermostat] // temp gained from heating
+        newInTemp = Math.round(newInTemp * 10) / 10 // round to one decimal
+        setIndoorTemp(newInTemp)
+      }
+    }, gameStepSize)
     return () => clearTimeout(timer)
    },
-   [gamePaused, gameTurn, gameStepSize]
+   [gamePaused, gameTurn, gameStepSize, indoorTemp, outdoorTemp]
   )
   
   function pauseGame(e) {
@@ -37,11 +73,11 @@ export default function App() {
 
   return (
   <MantineProvider>
-    <Grid align="center">
-    <Grid.Col span={2} align="right">
-      Current<br></br>temperature:
+    <Grid align="center" mt="10">
+    <Grid.Col span={3} align="right">
+      Current temperature:
     </Grid.Col>
-    <Grid.Col span={8} m="0" p="0">
+    <Grid.Col span={6} m="0" p="0">
       <Center align="center" m="0" p="0">
         <Box
           bg="linear-gradient(90deg, var(--mantine-color-blue-filled) 0%, var(--mantine-color-red-filled) 100%)"
@@ -50,10 +86,18 @@ export default function App() {
           p="0"
           h="2em"
         >
+          <Group justify="space-between">
+            <Text align="left" ml="10px">too cold</Text>
+            <Box bd="2px dotted black" m="-2px" h="2.2em" p="0px 2em 0px 2em">
+              <Text align="center">comfy</Text>
+            </Box>
+            <Text align="right" mr="10px">too warm</Text>
+          </Group>
         </Box>
       </Center>
       </Grid.Col>
-      <Grid.Col span={2}>
+      <Grid.Col span={3}>
+        <Text align="left">Inside: {indoorTemp}, outside: {outdoorTemp}</Text>
       </Grid.Col>
 
       <Grid.Col span={12}>
@@ -71,7 +115,7 @@ export default function App() {
           withLabel={false}
           size={300}
           thumbSize={30}
-          value={value}
+          value={thermostat}
           onChange={setThermostat}
           restrictToMarks
           marks={[
